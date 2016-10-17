e13d551388508ee007287a036472c52c56ffdb04
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2016-10-16 23:45:34 -0400

    Workshop6 inital commit

---
+++ in_lab/banking.c
@@ -0,0 +1,233 @@
+/*/*
+== Banking - Savings Account ==
+
+Author	:
+Date	:
+Revision:
+Workshop: #6 in-Lab
+
+*/
+
+#include <stdio.h>
+
+
+// Structure Declaration for account
+struct account
+{
+	int accNumber;				// Account number
+	double balance;				// Balance of the account
+};
+
+/*  Function to add an amount if positive-valued */
+//
+
+/* Function to calculate the interest rate*/
+//
+
+
+/* main program */
+int main()
+{
+	const int nosClients = 5;							// Number of accounts
+	int option;											// Hold the selection
+	// Initialize the struct account array with opening balance
+	struct account acct[] = { { 11111111, 123.45 },{ 22222222, 12365.50 },{ 33333333,0 },{ 44444444,1475 },{ 55555555,25000.65 } };
+
+	printf("***** Welcome to Savings Account Banking *****\n\n");
+
+	do
+	{
+		// Print the option list
+		printf("1.) Deposit \n");
+		printf("2.) Withdraw\n");
+		printf("3.) Apply monthly interest earnings to all accounts\n");
+		printf("4.) Apply service charges to all accounts\n");
+		printf("5.) Account Summary\n");
+		printf("0.) Log out\n\n");
+		printf("Please enter an option to continue: ");
+		
+		// Waiting for the input
+		scanf("%d",&option);
+
+		switch (option)
+		{
+		case 0: // Exit the program
+			break;
+
+		case 1: // Deposit
+				//@IN-LAB				
+
+
+			break;
+
+		case 2: // Withdraw funds
+				//@HOME
+
+
+			break;
+
+		case 3: // Apply interest earnings to all accounts
+				//@IN-LAB
+
+
+			break;
+
+		case 4: // Apply Service Charges
+				//@HOME
+
+
+			break;
+
+		case 5: // Display Account Information
+				//@IN-LAB
+
+
+			break;
+
+		default:
+			printf("Error: Please enter a valid option to continue\n\n");
+			break;
+		}
+
+
+	} while (option != 0);
+
+
+	return 0;
+}
+
+
+/* SAMPLE INPUT/OUTPUT EXPECTED */
+/*
+***** Welcome to Savings Account Banking *****
+
+1.) Deposit
+2.) Withdraw
+3.) Apply monthly interest earnings to all accounts
+4.) Apply service charges to all accounts
+5.) Account Summary
+0.) Log out
+
+Please enter an option to continue: 5
+
+-- Account information --
+
+Account# Balance
+-------- ----------
+11111111     123.45
+22222222   12365.50
+33333333       0.00
+44444444    1475.00
+55555555   25000.65
+
+1.) Deposit
+2.) Withdraw
+3.) Apply monthly interest earnings to all accounts
+4.) Apply service charges to all accounts
+5.) Account Summary
+0.) Log out
+
+Please enter an option to continue: 1
+
+-- Make a deposit --
+
+Enter account number: 67676767
+ERROR: Account number does not exist.
+
+1.) Deposit
+2.) Withdraw
+3.) Apply monthly interest earnings to all accounts
+4.) Apply service charges to all accounts
+5.) Account Summary
+0.) Log out
+
+Please enter an option to continue: 1
+
+-- Make a deposit --
+
+Enter account number: 44444444
+Enter amount to deposit (CAD): 20
+Current balance is : 1495.00
+
+1.) Deposit
+2.) Withdraw
+3.) Apply monthly interest earnings to all accounts
+4.) Apply service charges to all accounts
+5.) Account Summary
+0.) Log out
+
+Please enter an option to continue: 1
+
+-- Make a deposit --
+
+Enter account number: 11111111
+Enter amount to deposit (CAD): 450.67
+Current balance is : 574.12
+
+1.) Deposit
+2.) Withdraw
+3.) Apply monthly interest earnings to all accounts
+4.) Apply service charges to all accounts
+5.) Account Summary
+0.) Log out
+
+Please enter an option to continue: 5
+
+-- Account information --
+
+Account# Balance
+-------- ----------
+11111111     574.12
+22222222   12365.50
+33333333       0.00
+44444444    1495.00
+55555555   25000.65
+
+1.) Deposit
+2.) Withdraw
+3.) Apply monthly interest earnings to all accounts
+4.) Apply service charges to all accounts
+5.) Account Summary
+0.) Log out
+
+Please enter an option to continue: 3
+
+-- Add monthly interest earnings to all accounts --
+
+Account# New Balance Interest Earnings (M)
+-------- ----------- ---------------------
+11111111      583.65                  9.53
+22222222    12673.40                307.90
+33333333        0.00                  0.00
+44444444     1519.82                 24.82
+55555555    25623.17                622.52
+
+1.) Deposit
+2.) Withdraw
+3.) Apply monthly interest earnings to all accounts
+4.) Apply service charges to all accounts
+5.) Account Summary
+0.) Log out
+
+Please enter an option to continue: 5
+
+-- Account information --
+
+Account# Balance
+-------- ----------
+11111111     583.65
+22222222   12673.40
+33333333       0.00
+44444444    1519.82
+55555555   25623.17
+
+1.) Deposit
+2.) Withdraw
+3.) Apply monthly interest earnings to all accounts
+4.) Apply service charges to all accounts
+5.) Account Summary
+0.) Log out
+
+Please enter an option to continue: 0
+
+*/
